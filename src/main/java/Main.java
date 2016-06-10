acbfc53f4363b7c55d582900d3a2fcd593b0d0a5
Felix Beuster <felix.beuster@beusterse.de>
2016-06-10 19:07:52 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+}
