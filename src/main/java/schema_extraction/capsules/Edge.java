4f3c481b541769afb13de6c0574cbc583a7e81de
Felix Beuster <felix.beuster@beusterse.de>
2016-06-10 19:36:23 +0200

    added edge and node capsules

---
+++ src/main/java/schema_extraction/capsules/Edge.java
@@ -0,0 +1,80 @@
+package schema_extraction.capsules;
+
+import java.io.Serializable;
+import java.util.TreeSet;
+
+/**
+ * Created by Jakob Langner
+ * Implemented by Felix Beuster on 10.06.2016.
+ */
+public class Edge implements Serializable {
+
+    private int childLevel;
+
+    private static final long serialVersionUID = -8196526770077719084L;
+
+    private String childName    = "";
+    private String parentName   = "";
+
+    private TreeSet<String> docId = new TreeSet<>();
+
+    /**
+     * @return number of documents with this edge
+     */
+    public int countDoc_id() {
+        return this.docId.size();
+    }
+
+    /**
+     * Deletes the edge in a given document by deleting it's docId from this edge.
+     * @param docId document id
+     * @return Status of operation
+     */
+    public String deleteDocId(String docId) {
+        if (this.docId.contains(docId)) {
+            this.docId.remove(docId);
+
+            if (this.docId.size() == 0) {
+                return "NoDocumentsLeft";
+
+            } else {
+                return "Ok";
+            }
+
+        } else {
+            return "Error";
+        }
+    }
+
+    public int getChildLevel() {
+        return childLevel;
+    }
+
+    public String getChildName() {
+        return childName;
+    }
+
+    public TreeSet<String> getDocId() {
+        return docId;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setChildLevel(int childLevel) {
+        this.childLevel = childLevel;
+    }
+
+    public void setChildName(String childName) {
+        this.childName = childName;
+    }
+
+    public void setDocId(String docId) {
+        this.docId.add(docId);
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+}
---
+++ src/main/java/schema_extraction/capsules/Node.java
@@ -0,0 +1,88 @@
+package schema_extraction.capsules;
+
+import java.io.Serializable;
+import java.util.TreeSet;
+
+/**
+ * Created by Jakob Langner
+ * Implemented by Felix Beuster on 10.06.2016.
+ */
+public class Node implements Serializable {
+
+    private int level;
+
+    private static final long serialVersionUID = -4118580040968696269L;
+
+    private String name = "";
+
+    private TreeSet<String> docId       = new TreeSet<>();
+    private TreeSet<String> propType    = new TreeSet<>();
+
+    /**
+     * @return number of documents with this node
+     */
+    private int countDocId() {
+        return this.docId.size();
+    }
+
+    /**
+     * deletes this node in the given document by deleting its docID from this node
+     * @param docId document id
+     * @return Status of the operation
+     */
+    public String deleteDocId(String docId) {
+        if (this.docId.contains(docId)) {
+            this.docId.remove(docId);
+
+            if (this.docId.size() == 0) {
+                return "NoDocumentsLeft";
+
+            } else {
+                return "Ok";
+            }
+
+        } else {
+            return "Error";
+        }
+    }
+
+    public TreeSet<String> getDocId() {
+        return docId;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TreeSet<String> getPropType() {
+        return propType;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public void setDocId(String docId) {
+        if (this.docId.contains(docId) == false) {
+            this.docId.add(docId);
+        }
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPropType(String propType) {
+        if (this.propType.contains(propType) == false) {
+            this.propType.add(propType);
+        }
+    }
+}
